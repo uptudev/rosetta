5adbee307c84e5c42cf16ef60e265d25daf7264b
uptu <91502176+uptudev@users.noreply.github.com>
2023-05-28 16:42:44 -0600

    Update main.rs
    why the fuck was I using signed integers????????
    
    don't write code while tired folks.

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -1,7 +1,7 @@
 pub fn fibonacci() {
-    let mut a: i64 = 0;
+    let mut a: u64 = 0;
     println!("{:?}", a);
-    let mut b: i64 = 1;
+    let mut b: u64 = 1;
     println!("{:?}", b);
 
     while let Some(c) = a.checked_add(b) {
