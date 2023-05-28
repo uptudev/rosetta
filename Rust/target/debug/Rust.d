7386f9d574cd2347a43e5016695a82d31dcc7960
uptu <uptu@uptu.dev>
2023-05-28 01:00:15 -0600

    removed everything

--- Rust/target/debug/Rust.d
+++
@@ -1,1 +0,0 @@
-/home/uptu/dev/rosetta/Rust/target/debug/Rust: /home/uptu/dev/rosetta/Rust/src/main.rs
--- Rust/target/debug/deps/Rust-2af7890b9fbe8654.d
+++
@@ -1,5 +0,0 @@
-/home/uptu/dev/rosetta/Rust/target/debug/deps/Rust-2af7890b9fbe8654.rmeta: src/main.rs
-
-/home/uptu/dev/rosetta/Rust/target/debug/deps/Rust-2af7890b9fbe8654.d: src/main.rs
-
-src/main.rs:
