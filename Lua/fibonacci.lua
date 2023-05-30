0b1f4d59c65c50d4b1b7d88b78cb78e2c03e554b
uptu <91502176+uptudev@users.noreply.github.com>
2023-05-30 12:31:53 -0600

    Remove comments in lua

--- Lua/fibonacci.lua
+++ Lua/fibonacci.lua
@@ -1,9 +1,3 @@
--- Lua >5.3 officially supports only signed 64-bit values with the `integer` subtype.
--- As such, even though the `string.format` call below interprets it as a u64,
--- the internal value is still calculated via i64 math.
--- Because of this, technically the i64 will "overflow" one iteration early,
--- so we have to account for this with control flow even if the formatter can "pretend" its a u64.
-
 local a = math.tointeger(0)
 print(a)
 local b = math.tointeger(1)
@@ -24,4 +18,3 @@ do
     a = b
     b = c
 end
-
