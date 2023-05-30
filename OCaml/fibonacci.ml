ebf564b36f36c8a07ee025638cac4585ce417cc4
uptu <91502176+uptudev@users.noreply.github.com>
2023-05-30 00:56:18 -0600

    Update fibonacci.ml
    It's so short that the comments really make it *harder* to read.

--- OCaml/fibonacci.ml
+++ OCaml/fibonacci.ml
@@ -1,11 +1,7 @@
-(* declaring recursive function `fibonacci` *)
 let rec fibonacci a b =
-  (* pattern match based on the sum of a and b, returning the result as `c` *)
   match Int64.add a b with
-  (* using unsigned comparison, ensure `c` hasn't overflowed *)
   | c when Int64.unsigned_compare c b < 0 ->
       print_endline "Overflowed!"
-  (* else just print c and recurse *)
   | c ->
       Printf.printf "%Lu\n" c;
       fibonacci b c
