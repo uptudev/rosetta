dd3e15c503e21129c53ef9a8fd82b37720e5e661
uptu <uptu@uptu.dev>
2023-06-27 01:21:57 -0600

    Added Go solution

---
+++ Go/fibonacci.go
@@ -0,0 +1,26 @@
+package main
+import "fmt"
+
+func fibonacci() {
+    var a uint64 = 0;
+    fmt.Println(a)
+    var b uint64 = 1;
+    fmt.Println(b)
+
+    for true {
+        var c uint64 = a + b
+
+        if c < b {
+            fmt.Println("Overflowed!")
+            break
+        }
+
+        fmt.Println(c)
+        a = b
+        b = c
+    }
+}
+
+func main() {
+    fibonacci()
+}
