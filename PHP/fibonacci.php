f5322fb08c9e217bd22e650a75ea9a1abbfa0539
uptu <uptu@uptu.dev>
2023-10-11 11:25:46 -0600

    Make PHP solution concise
    No need for an extra `$maximum` value when it can be hardcoded.

--- PHP/fibonacci.php
+++ PHP/fibonacci.php
@@ -1,11 +1,10 @@
 <?php
-$maximum = 18446744073709551615;
 $a = 0;
 echo "$a\n";
 $b = 1;
 echo "$b\n";
 
-while(($a + $b) < $maximum) {
+while(($a + $b) < 18446744073709551615) {
     $c = $a + $b;
     $cstr = sprintf('%lu', $c);    
     echo "$cstr\n";
