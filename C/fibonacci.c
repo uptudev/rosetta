62b00dbb439846b149dec2bb1ccd20bb3c0cb009
uptu <uptu@uptu.dev>
2023-10-13 02:09:15 -0600

    Remove boolean import in C solution
    C has no native support for boolean values, as `true` must be imported via `#include <stdbool.h>`, which is unnecessary given `1` works identically to `true`.
    
    Removed the import and used `while (1) {}` instead.

--- C/fibonacci.c
+++ C/fibonacci.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdbool.h>
 
 int main() {
     unsigned long long a = 0;
@@ -7,7 +6,7 @@ int main() {
     unsigned long long b = 1;
     printf("%llu\n", b);
 
-    while (true) {
+    while (1) {
         unsigned long long c = a + b;
 
         if (c < b) {
