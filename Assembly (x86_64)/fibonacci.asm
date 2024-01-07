c486a853d04525e97330985e4c564d95b8bfa43c
uptu <uptu@uptu.dev>
2024-01-06 17:32:18 -0700

    Optimized and documented Assembly solution

--- Assembly (x86_64)/fibonacci.asm
+++ Assembly (x86_64)/fibonacci.asm
@@ -1,74 +1,73 @@
-.data
-    format_string:
-        .string "%llu\n"
-    overflow_alert_string:
-        .string "Overflowed!"
-
 .text
-    .global main
+	.globl	main                            
 
-fibonacci:
-        pushq   %rbp
-        movq    %rsp, %rbp
-        subq    $32, %rsp
+    # Entry point
+	.p2align	4, 0x90
+	.type	main,@function
+main:                                   
+    # push register values to the stack
+	pushq	%r14
+	pushq	%rbx
+	pushq	%rax
 
-        movq    $0, -8(%rbp)
+    # print `0`
+	movl	$.format_str, %edi
+	xorl	%esi, %esi
+	xorl	%eax, %eax
+	callq	printf
 
-        movq    -8(%rbp), %rax
-        movq    %rax, %rsi
-        movl    $format_string, %edi
-        movl    $0, %eax
-        call    printf
+    # print `1`
+	movl	$1, %r14d
+	movl	$.format_str, %edi
+	movl	$1, %esi
+	xorl	%eax, %eax
+	callq	printf
 
-        movq    $1, -16(%rbp)
-        
-        movq    -16(%rbp), %rax
-        movq    %rax, %rsi
-        movl    $format_string, %edi
-        movl    $0, %eax
-        call    printf
+    # initialize loop variable
+	movl	$1, %ebx
 
-    checked_add_u64:
-        movq    -8(%rbp), %rdx
-        movq    -16(%rbp), %rax
+    # addition + printing loop 
+	.p2align	4, 0x90
+.loop:
+    # print current value
+	movl	$.format_str, %edi
+	movq	%rbx, %rsi
+	xorl	%eax, %eax
+	callq	printf
 
-        addq    %rdx, %rax
+    # add values, jumping back up the loop unless the Carry Flag is true (indicates overflow)
+	movq	%r14, %rax
+	movq	%rbx, %r14
+	addq	%rbx, %rax
+	movq	%rax, %rbx
+	jae	.loop
 
-        movq    %rax, -24(%rbp)
+    # print overflow string
+	movl	$.overflow_str, %edi
+	callq	puts@PLT
 
-        cmpq    -16(%rbp), %rax
+    # clear `%eax` and increment stack pointer
+	xorl	%eax, %eax
+	addq	$8, %rsp
 
-        jnb     increment
+    # pop old values back into the registers and return out
+	popq	%rbx
+	popq	%r14
+	retq
 
-        movl    $overflow_alert_string, %edi
-        call    puts
-        jmp     clean_up
+    # Calculates main function size for the linker
+.end:
+	.size	main, .end-main
 
-    
-    increment:
-        movq    -24(%rbp), %rax
-        movq    %rax, %rsi
-        movl    $format_string, %edi
-        movl    $0, %eax
-        call    printf
+    # Format string for u64 -> String conversion
+	.type	.format_str,@object
+	.section	.rodata.str1.1,"aMS",@progbits,1
+.format_str:
+	.asciz	"%llu\n"
+	.size	.format_str, 6
 
-        movq    -16(%rbp), %rax
-        movq    %rax, -8(%rbp)
-        movq    -24(%rbp), %rax
-        movq    %rax, -16(%rbp)
-
-        jmp     checked_add_u64
-
-    clean_up:
-        nop
-        leave
-        ret
-
-main:
-        pushq   %rbp
-        movq    %rsp, %rbp
-        movl    $0, %eax
-        call    fibonacci
-        movl    $0, %eax
-        popq    %rbp
-        ret
+    # String to print when integer overflows
+	.type	.overflow_str,@object
+.overflow_str:
+	.asciz	"Overflowed!"
+	.size	.overflow_str, 12
