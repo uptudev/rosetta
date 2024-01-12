aa2ebbd44f0d4605809ad5406f0779f0daef5728
uptu <uptu@uptu.dev>
2024-01-12 00:05:57 -0700

    Trimmed a whole bunch of fat off of my Rust MP solution

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -8,13 +8,15 @@ fn fibonacci(input: usize) {
     let mut b = Natural::ONE;
     let mut c = Natural::ONE;
     match input {
-        0 => {}
-        1 => {println!("0")},
-        2 | 3 => {println!("1")},
+        0 => {},                        // not valid input here; do nothing and terminate
+        1 => {println!("0")},           // these two lines are cheaper than converting to a string...
+        2 | 3 => {println!("1")},       // ...as only numbers above 3 require computation as is      
         _ => {
-            for _ in 0..(input-3) {
-                a = replace(&mut b, c);
-                c = a + b.clone();
+            let mut i = input - 3;      // init counter for a makeshift C-like for loop
+            while i > 0 {               // no need for a Rust for loop, as an iterator could create overhead
+                a = replace(&mut b, c); // moves all values at once (a <- b <- c)
+                c = a + b.clone();      // get new value for `c`
+                i -= 1;                 // decrement counter
             }
             println!("{}", c.to_string_base(10));
         },
@@ -22,48 +24,25 @@ fn fibonacci(input: usize) {
 }
 
 fn main() {
-    println!("Which fibonacci number do you wish to calculate?");
-    let mut str = String::new();
-    stdin().read_line(&mut str)
+    println!("Which fibonacci number do you wish to calculate?");   
+    let mut str = String::new();            // initialize input buffer
+    stdin().read_line(&mut str)             // push input to buffer
         .expect("Failed to read input.");
-    let num = atoi(str);
-
     println!("");
-    fibonacci(num);
+
+    let n = atoi(str);                      // convert string to usize
+    fibonacci(n);                           // calculate nth fibonacci number
 }
 
-fn atoi(s: String) -> usize {
-    let mut s = s.trim_start().to_string();
-    if s.is_empty() {return 0;}
-
-    // check if first character is a digit
-    if let Some(c) = s.chars().next() {
-        if !c.is_digit(10) {
-            return 0;
-        }
-    } else {
-        return 0;
+fn atoi(mut s: String) -> usize {
+    s = s.trim_start().to_string();
+    if let Some(i) = s.chars().position(|c| !c.is_digit(10)) {
+        s.truncate(i);              // truncate to first nondecimal character encountered
     }
-
-    let mut i = 0;
-    
-    while i < s.len() {
-        if !s.chars().nth(i).unwrap().is_digit(10) {
-            s.truncate(i);
-            break;
-        }
-        i += 1;
-    } 
-    parse_string_to_usize(&s)
-}
-
-fn parse_string_to_usize(s: &str) -> usize {
-    match s.parse::<usize>() {
-        Ok(num) => {
-            num
-        },
-        Err(_) => {
-            usize::MIN
-        }
+    if s.is_empty() {return 0;}     // early return
+        
+    match s.parse::<usize>() {      // parse and return result (or 0 if an error occured)
+        Ok(num) => num,
+        Err(_) => 0,
     }
 }
