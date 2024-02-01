96eda0fcb8d8046920f42b444124faef2dcafe39
uptu <uptu@uptu.dev>
2024-02-01 00:37:15 -0700

    Modified Zig solution and C solutions

---
+++ C/fib_rec.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int rec_fib(unsigned long long a, unsigned long long b) {
+    unsigned long long c = a + b;
+    if (c < b) {
+        puts("Overflowed!\n");
+        return 0;
+    }
+
+    printf("%llu\n", c);
+    rec_fib(b, c);
+}
+
+int main() {
+    unsigned long long a = 0;
+    unsigned long long b = 1;
+    printf("%llu\n", a);
+    printf("%llu\n", b);
+
+    return rec_fib(a, b);
+}
--- C/fibonacci_gmp.c
+++ C/fibonacci_gmp.c
@@ -8,12 +8,10 @@ int main() {
 
     switch (input) {
         case 0:
-            return 0;
-        case 1:
             puts("\n0");
             return 0;
+        case 1:
         case 2:
-        case 3:
             puts("\n1");
             return 0;
         default:
@@ -30,7 +28,7 @@ int main() {
     mpz_t c;
     mpz_init_set_ui(c, 1);
 
-    for (unsigned long long i = 3; i < input; i++) {
+    for (unsigned long long i = 2; i < input; i++) {
         mpz_set(a, b);
         mpz_set(b, c);
         mpz_add(c, a, b);
@@ -38,4 +36,8 @@ int main() {
 
     gmp_printf("%Zd\n", c);
     return 0;
+}
+
+unsigned long long fib(unsigned long long a, unsigned long long b) {
+
 }
