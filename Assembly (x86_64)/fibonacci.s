f4cc930615bd1c4362a8785a47dbba1d94cb6a43
uptu <uptu@uptu.dev>
2024-05-19 02:21:04 -0600

    Switched Assembly syntax to Intel for readability

--- Assembly (x86_64)/fibonacci.s
+++ Assembly (x86_64)/fibonacci.s
@@ -1,72 +1,75 @@
-.text
-	.globl	main                            
+.global main
+.intel_syntax noprefix
 
-    # Entry point
-	.p2align	4, 0x90
-	.type	main,@function
-main:                                   
-    # push register values to the stack
-	pushq	%r14
-	pushq	%rbx
-	xorq    %rax, %rax
+# Entry point
+.p2align	4, 0x90
+.type	main,@function
+main:
+    # Save registers and zero out rax
+    push    r14
+    push    rbx
+    xor     rax, rax
 
     # print `0`
-	movl	$.format_str, %edi
-	xorl	%esi, %esi
-	xorl	%eax, %eax
-	callq	printf
+    mov     edi, offset .format_str
+    xor     esi, esi
+    xor     eax, eax
+    call    printf
 
     # print `1`
-	movl	$1, %r14d
-	movl	$.format_str, %edi
-	movl	$1, %esi
-	xorl	%eax, %eax
-	callq	printf
+    mov     r14d, 1
+    mov     edi, offset .format_str
+    mov     esi, 1
+    xor     eax, eax
+    call    printf
 
     # initialize loop variable
-	movl	$1, %ebx
+    mov     ebx, 1
 
-    # addition + printing loop 
-	.p2align	4, 0x90
+# loop procedure
+.p2align    4, 0x90
 .loop:
     # print current value
-	movl	$.format_str, %edi
-	movq	%rbx, %rsi
-	xorl	%eax, %eax
-	callq	printf
+    mov     edi, offset .format_str
+    mov     rsi, rbx
+    xor     eax, eax
+    call    printf
 
-    # add values, jumping back up the loop unless the Carry Flag is true (indicates overflow)
-	movq	%r14, %rax
-	movq	%rbx, %r14
-	addq	%rbx, %rax
-	movq	%rax, %rbx
-	jae	.loop
+    # add values, jumping up the loop unless the carry flag is set (indicating overflow)
+    mov     rax, r14
+    mov     r14, rbx
+    add     rax, rbx
+    mov     rbx, rax
+    jae     .loop
 
-    # print overflow string
-	movl	$.overflow_str, %edi
-	callq	puts
+    # print overflow message
+    mov     rax, 1
+    mov     rdi, 1
+    mov     rsi, offset .overflow_str
+    mov     rdx, 13
+    syscall
 
-    # clear `%eax` and increment stack pointer
-	xorl	%eax, %eax
+    # clear registers, pop old registers, and exit
+    xor     rax, rax
+    xor     rdi, rdi
+    xor     rsi, rsi
+    xor     rdx, rdx
+    pop     rbx
+    pop     r14
+    ret
 
-    # pop old values back into the registers and return out
-	popq	%rbx
-	popq	%r14
-	retq
-
-    # Calculates main function size for the linker
 .end:
-	.size	main, .end-main
+    .size main, .end-main
 
-    # Format string for u64 -> String conversion
+# Format string for u64 -> String conversion
 	.type	.format_str,@object
 	.section	.rodata.str1.1,"aMS",@progbits,1
 .format_str:
 	.asciz	"%llu\n"
 	.size	.format_str, 6
 
-    # String to print when integer overflows
+# String to print when integer overflows
 	.type	.overflow_str,@object
 .overflow_str:
-	.asciz	"Overflowed!"
-	.size	.overflow_str, 12
+	.asciz	"Overflowed!\n"
+	.size	.overflow_str, 13
---
+++ Assembly (x86_64)/fibonacci_at&t.s
@@ -0,0 +1,72 @@
+.text
+	.globl	main                            
+
+    # Entry point
+	.p2align	4, 0x90
+	.type	main,@function
+main:                                   
+    # push register values to the stack
+	pushq	%r14
+	pushq	%rbx
+	xorq    %rax, %rax
+
+    # print `0`
+	movl	$.format_str, %edi
+	xorl	%esi, %esi
+	xorl	%eax, %eax
+	callq	printf
+
+    # print `1`
+	movl	$1, %r14d
+	movl	$.format_str, %edi
+	movl	$1, %esi
+	xorl	%eax, %eax
+	callq	printf
+
+    # initialize loop variable
+	movl	$1, %ebx
+
+    # addition + printing loop 
+	.p2align	4, 0x90
+.loop:
+    # print current value
+	movl	$.format_str, %edi
+	movq	%rbx, %rsi
+	xorl	%eax, %eax
+	callq	printf
+
+    # add values, jumping back up the loop unless the Carry Flag is true (indicates overflow)
+	movq	%r14, %rax
+	movq	%rbx, %r14
+	addq	%rbx, %rax
+	movq	%rax, %rbx
+	jae	.loop
+
+    # print overflow string
+	movl	$.overflow_str, %edi
+	callq	puts
+
+    # clear `%eax` and increment stack pointer
+	xorl	%eax, %eax
+
+    # pop old values back into the registers and return out
+	popq	%rbx
+	popq	%r14
+	retq
+
+    # Calculates main function size for the linker
+.end:
+	.size	main, .end-main
+
+    # Format string for u64 -> String conversion
+	.type	.format_str,@object
+	.section	.rodata.str1.1,"aMS",@progbits,1
+.format_str:
+	.asciz	"%llu\n"
+	.size	.format_str, 6
+
+    # String to print when integer overflows
+	.type	.overflow_str,@object
+.overflow_str:
+	.asciz	"Overflowed!"
+	.size	.overflow_str, 12
