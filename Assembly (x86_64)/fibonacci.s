32315b5e4cd9611e3797cc3157618462d730bb52
uptu <91502176+uptudev@users.noreply.github.com>
2024-09-16 23:12:39 -0700

    Consistent tabs/spaces

--- Assembly (x86_64)/fibonacci.s
+++ Assembly (x86_64)/fibonacci.s
@@ -13,7 +13,7 @@
 
 # Entry point
 main:
-    # Push old values onto the stack and zero out syscall registers
+# Push old values onto the stack and zero out syscall registers
 	push 	r13
 	push 	r12
 	push 	rbp
@@ -21,62 +21,62 @@ main:
 	xor 	rsi, rsi
 	xor 	rax, rax
 
-    # Format string address into r13 for later use
-    lea     r13, [rip + .fmt_str]
+# Format string address into r13 for later use
+	lea     r13, [rip + .fmt_str]
 
-    # Print `0`
+# Print `0`
 	mov 	rdi, r13    # load format string address into rdi
 	mov 	r12, 1      # load 1 into r12
 	mov 	rbx, 1      # load 1 into rbx
 	sub 	rsp, 8      # subtract 8 from rsp to align stack
 	call	printf      # call printf
 
-    # Print `1`
+# Print `1`
 	mov 	rsi, 1      # load 1 into rsi (was 0 before)
 	mov 	rdi, r13    # reload format string address into rdi
 	call	printf      # call printf
 
-    # Loop goto
+# Loop goto
 .loop:
-    # Print rbx
+# Print rbx
 	mov 	rsi, rbx
 	mov 	rdi, r13
 	call	printf  
 
-    # Add rbx and r12 into rbp; check for overflow
+# Add rbx and r12 into rbp; check for overflow
 	mov 	rbp, r12
 	add 	rbp, rbx
-	jc	    .overflow
+	jc	.overflow
 
-    # Print rbp
+# Print rbp
 	mov 	rsi, rbp
 	mov 	rdi, r13
 	call	printf  
 
-    # Add rbp and rbx into r12; check for overflow
+# Add rbp and rbx into r12; check for overflow
 	mov 	r12, rbx
 	add 	r12, rbp
-	jc	    .overflow
+	jc	.overflow
 
-    # Print r12
-    mov 	rsi, r12
+# Print r12
+    	mov 	rsi, r12
 	mov 	rdi, r13
 	call	printf
 
-    # Add r12 and rbp into rbx; check for overflow
-    mov     rbx, rbp
-    add     rbx, r12
-	jnc	    .loop
+# Add r12 and rbp into rbx; check for overflow
+    	mov     rbx, rbp
+   	add     rbx, r12
+	jnc	.loop
 
 .overflow:
-    # Print overflow string
-    lea     rdi, [rip + .overflow_str]
+# Print overflow string
+    	lea     rdi, [rip + .overflow_str]
 	call	puts
 
-    # Align stack
-    add     rsp, 8
+# Align stack
+    	add     rsp, 8
 
-    # Reset registers and return
+# Reset registers and return
 	xor 	rax, rax
 	pop 	rbx
 	pop 	rbp
