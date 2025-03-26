ea2974983ae3576f10dbefe065f5b93c8100d82b
uptu <uptu@uptu.dev>
2025-03-26 04:34:30 -0700

    this is literally the same code why

--- Assembly (ARM64)/fibonacci.s
+++ Assembly (ARM64)/fibonacci.s
@@ -1,45 +1,37 @@
-// Constants
-.section    .rodata
+.text
+.globl main
+main:
+    stp x29, x30, [sp, #-48]!
+    str x21, [sp, #16]
+    stp x20, x19, [sp, #32]
+    mov x29, sp
+    
+    adrp x19, .fmt
+    add x19, x19, :lo12:.fmt
+
+    mov x1, xzr
+    mov x0, x19
+    bl printf
+
+    mov x0, x19
+    mov w1, #1
+    mov w21, #1
+    bl printf
+    mov w20, #1
+
+.loop:
+    mov x0, x19
+    mov x1, x10
+    bl printf
+
+    mov w0, wzr
+    ldp x10, x11, [sp, #32]
+    ldr x12, [sp, #16]
+    ldp x29, x30, [sp], #48
+    ret
+
+    .section    .rodata
 .fmt:
     .asciz "%llu\n"
 .overflow:
     .asciz "Overflowed!"
-
-// Code
-.text
-.globl main
-main:
-    // Push frame pointer and link register to stack; increment sp by 16
-    // Frame pointer is now the stack pointer value
-    stp x29, x30, [sp, -0x30]!  // Frame pointer and link register
-    str x12, [sp, 0x10]         // String address registers
-    stp x10, x11, [sp, 0x20]    // Calculation registers
-    mov x29, sp
-    
-    // Load effective addresses of strings into x0 (via x64 bitmask trickery)
-    adrp x12, .fmt
-    add x12, x12, :lo12:.fmt
-
-    // Print `0`
-    mov x0, x12
-    mov x1, xzr
-    bl printf
-
-    // Print `1`
-    mov x0, x12
-    mov w1, #1
-    bl printf
-    mov w10. #1
-    mov w11. #1
-
-.loop:
-    mov x0, x12
-    mov x1, x10
-    bl printf
-
-    // End of program; pop old register values
-    mov w0, wzr
-    ldp x10, x11, [sp, 0x20]
-    ldr x12, [sp, 0x10]
-    ldp x29, x30, [sp], 0x30
-    ret
